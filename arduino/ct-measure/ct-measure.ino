2ab09ab570210e387e9a6f9b2b3bb182936c5b64
Th1nkK1D <witheep@gmail.com>
2021-02-27 17:35:36 +0700

    Update ct measure logic and compare with emonlib

--- arduino/ct-measure/ct-measure.ino
+++ arduino/ct-measure/ct-measure.ino
@@ -1,23 +1,38 @@
-int INPUT_PIN = A0;
-int SAMPLING_SIZE = 10000;
-double CALIBRATION_VALUE = 511.72;
-int CT_RATIO = 30;
+#include "EmonLib.h"
+EnergyMonitor emon1; 
+
+int INPUT_PIN = 0;
+int SAMPLING_SIZE = 1480;
+double CT_CURRENT_VOLTAGE_RATIO = 30;
+double INPUT_CALIBRATION = 509;
+double EMON_CALIBRATION = 1800/62;
+
+double voltageMultiplier = CT_CURRENT_VOLTAGE_RATIO * 10 / 512;
 
 void setup() {
   Serial.begin(9600);
+  emon1.current(INPUT_PIN, EMON_CALIBRATION);
+}
+
+double calcIrms(int samplingSize) {
+  double sum = 0;
+
+  for (int i = 0; i < samplingSize; i++){
+    double inputVoltage = analogRead(INPUT_PIN);
+    double current = (inputVoltage - INPUT_CALIBRATION) * voltageMultiplier;
+    sum += current * current;
+  }
+
+  return sqrt(sum / samplingSize);
 }
 
 void loop() {
-  double sum = 0;
-
-  for (int i = 0; i < SAMPLING_SIZE; i++){
-    double value = analogRead(INPUT_PIN);
-    sum += value * value;
-  }
-
-  double rms = sqrt(sum / SAMPLING_SIZE);
-  double voltage = rms - CALIBRATION_VALUE;
-  double current = voltage * CT_RATIO;
+  double manualIrms = calcIrms(SAMPLING_SIZE);
+  double emonIrms = emon1.calcIrms(SAMPLING_SIZE);
   
-  Serial.println(current);
+  Serial.print("manual: ");
+  Serial.print(manualIrms);
+  Serial.print(" | emonLib: ");
+  Serial.print(emonIrms);
+  Serial.print('\n');
 }
